d86904c5b6080189daf769624aa297b93802e2d0
飞鸽巴巴塔 <feigebabata@outlook.com>
2026-05-25 15:30:41 +0800

    Refactor TryInject and injectField methods for object

--- Gameplay/Runtime/DI/PartDIContainer.cs
+++ Gameplay/Runtime/DI/PartDIContainer.cs
@@ -15,34 +15,34 @@ namespace FGUFW.Gameplay
         // protected bool initialed;
         protected Dictionary<Type,object> defaultDependency= new();
 
-        public async UniTask TryInject(Part part,CancellationToken partTaskCancellationToken)
+        public async UniTask TryInject(object obj,CancellationToken partTaskCancellationToken)
         {
             // await waitInitialed();
 
-            foreach (FieldInfo f_info in part.GetType().GetFields (BindingFlags.Instance | BindingFlags.Public  | BindingFlags.NonPublic))
+            foreach (FieldInfo f_info in obj.GetType().GetFieldsByCache())
             {
                 if(partTaskCancellationToken.IsCancellationRequested)break;
 
                 var injectConfig = f_info.GetCustomAttribute<InjectAttribute>();
                 if(injectConfig!=default)
                 {
-                    await injectField(part,f_info,injectConfig.Field,partTaskCancellationToken);
+                    await injectField(obj,f_info,injectConfig.Field,partTaskCancellationToken);
                 }
             }
         }
 
-        private async UniTask injectField(Part part,FieldInfo f_info, InjectField field,CancellationToken partTaskCancellationToken)
+        private async UniTask injectField(object obj,FieldInfo f_info, InjectField field,CancellationToken partTaskCancellationToken)
         {
             switch (field)
             {
                 case InjectField.Default:
-                    injectDefaultField(part,f_info);
+                    injectDefaultField(obj,f_info);
                 break;
                 case InjectField.UI:
-                    await injectUIField(part,f_info,partTaskCancellationToken);
+                    await injectUIField(obj,f_info,partTaskCancellationToken);
                 break;
                 case InjectField.Save:
-                    injectSaveField(part,f_info);
+                    injectSaveField(obj,f_info);
                 break;
             }
         }
@@ -56,9 +56,9 @@ namespace FGUFW.Gameplay
         //     }
         // }
 
-        protected abstract UniTask injectUIField(Part part,FieldInfo f_info,CancellationToken partTaskCancellationToken);
+        protected abstract UniTask injectUIField(object obj,FieldInfo f_info,CancellationToken partTaskCancellationToken);
 
-        protected void injectDefaultField(Part part,FieldInfo f_info)
+        protected void injectDefaultField(object obj,FieldInfo f_info)
         {
             var fieldType = f_info.FieldType;
             object fieldData = default;
@@ -68,14 +68,14 @@ namespace FGUFW.Gameplay
                 Debug.LogWarning($"{name}未注册{fieldType.FullName}!");
                 return;
             }
-            f_info.SetValue(part,fieldData);
+            f_info.SetValue(obj,fieldData);
         }
 
-        protected void injectSaveField(Part part,FieldInfo f_info)
+        protected void injectSaveField(object obj,FieldInfo f_info)
         {
             var fieldType = f_info.FieldType;
             object fieldData = PartSaveUtility.Get(fieldType);
-            f_info.SetValue(part,fieldData);
+            f_info.SetValue(obj,fieldData);
         }
 
         public static void DestroyPlay<T>() where T: Play
