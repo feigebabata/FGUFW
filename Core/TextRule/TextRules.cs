472b0b90fb8e4bedab371a8fc465e80688823b71
飞鸽巴巴塔 <feigebabata@outlook.com>
2026-06-04 18:59:28 +0800

    Refactor TextRules class fields and methods

--- Core/TextRule/TextRules.cs
+++ Core/TextRule/TextRules.cs
@@ -1,16 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace FGUFW
 {
     public class TextRules
     {
-        public char Rule;
-        public bool Variate;
-        public float Value;
-        public List<TextRules> Children;
+        /// <summary>
+        /// 符号 计算/比较
+        /// </summary>
+        private char rule;
+
+        /// <summary>
+        /// 是否为变量
+        /// </summary>
+        private bool isVariate;
+        private float value;
+        private List<TextRules> children;
 
 
         public TextRules(object variateGet, ReadOnlySpan<char> text)
@@ -28,15 +37,15 @@ namespace FGUFW
             {
                 if (text[0] == TextRulesHelper.Code_Variate)//变量
                 {
-                    Variate = true;
-                    Value = VariateGetUtility.GetVariateKey(variateGet, text.Slice(1).ToString());
+                    isVariate = true;
+                    value = VariateGetUtility.GetVariateKey(variateGet, text.Slice(1).ToString());
                 }
                 else//常量
                 {
-                    Variate = false;
+                    isVariate = false;
                     try
                     {
-                        Value = text.ToFloat();
+                        value = text.ToFloat();
                     }
                     catch (System.Exception)
                     {
@@ -46,55 +55,55 @@ namespace FGUFW
             }
             else if (TextRulesHelper.FindCodeOR(text, 0) != -1)// 或
             {
-                Children = new List<TextRules>(2);
+                children = new List<TextRules>(2);
                 int idx = TextRulesHelper.FindCodeOR(text, 0);
-                Rule = text[idx];
-                Children.Add(new TextRules(variateGet, text.Slice(0, idx)));
-                Children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
+                rule = text[idx];
+                children.Add(new TextRules(variateGet, text.Slice(0, idx)));
+                children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
             }
             else if (TextRulesHelper.FindCodeAND(text, 0) != -1)// 且
             {
-                Children = new List<TextRules>(2);
+                children = new List<TextRules>(2);
                 int idx = TextRulesHelper.FindCodeAND(text, 0);
-                Rule = text[idx];
-                Children.Add(new TextRules(variateGet, text.Slice(0, idx)));
-                Children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
+                rule = text[idx];
+                children.Add(new TextRules(variateGet, text.Slice(0, idx)));
+                children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
             }
             else if (TextRulesHelper.FindEqualCode(text, 0) != -1)//比较
             {
-                Children = new List<TextRules>(2);
+                children = new List<TextRules>(2);
                 int idx = TextRulesHelper.FindEqualCode(text, 0);
-                Rule = text[idx];
-                Children.Add(new TextRules(variateGet, text.Slice(0, idx)));
-                Children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
+                rule = text[idx];
+                children.Add(new TextRules(variateGet, text.Slice(0, idx)));
+                children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
             }
             else if (TextRulesHelper.FindComputeL2Code(text, 0) != -1)//加减
             {
-                Children = new List<TextRules>(2);
+                children = new List<TextRules>(2);
                 int idx = TextRulesHelper.FindComputeL2Code(text, 0);
-                Rule = text[idx];
-                Children.Add(new TextRules(variateGet, text.Slice(0, idx)));
-                Children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
+                rule = text[idx];
+                children.Add(new TextRules(variateGet, text.Slice(0, idx)));
+                children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
             }
             else if (TextRulesHelper.FindComputeL1Code(text, 0) != -1)//乘除取余
             {
-                Children = new List<TextRules>(2);
+                children = new List<TextRules>(2);
                 int idx = TextRulesHelper.FindComputeL1Code(text, 0);
-                Rule = text[idx];
-                Children.Add(new TextRules(variateGet, text.Slice(0, idx)));
-                Children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
+                rule = text[idx];
+                children.Add(new TextRules(variateGet, text.Slice(0, idx)));
+                children.Add(new TextRules(variateGet, text.Slice(idx + 1)));
             }
             else if (TextRulesHelper.FindInOutCode(text) != -1) //集合
             {
-                Children = new List<TextRules>();
+                children = new List<TextRules>();
                 int idx = TextRulesHelper.FindInOutCode(text);
-                Rule = text[idx];
-                Children.Add(new TextRules(variateGet, text.Slice(0, idx)));
+                rule = text[idx];
+                children.Add(new TextRules(variateGet, text.Slice(0, idx)));
 
                 var rights = text.Slice(idx + 1).ToString().Split(TextRulesHelper.Code_Split);
                 foreach (var item in rights)
                 {
-                    Children.Add(new TextRules(variateGet, item));
+                    children.Add(new TextRules(variateGet, item));
                 }
 
             }
@@ -109,17 +118,17 @@ namespace FGUFW
         public float GetValue(object variateGet)
         {
             float result = 0;
-            switch (Rule)
+            switch (rule)
             {
                 case TextRulesHelper.Code_Value:
                     {
-                        result = VariateGetUtility.GetValue(variateGet, Variate, Value);
+                        result = VariateGetUtility.GetValue(variateGet, isVariate, value);
                     }
                     break;
                 //----------------------------------------------------
                 case TextRulesHelper.Code_Or:
                     {
-                        foreach (var child in Children)
+                        foreach (var child in children)
                         {
                             if (child.GetValue(variateGet) == 1)
                             {
@@ -132,7 +141,7 @@ namespace FGUFW
                 case TextRulesHelper.Code_And:
                     {
                         result = 1;
-                        foreach (var child in Children)
+                        foreach (var child in children)
                         {
                             if (child.GetValue(variateGet) == 0)
                             {
@@ -146,12 +155,12 @@ namespace FGUFW
                 case TextRulesHelper.Code_In:
                     {
                         result = 0;
-                        var left = Children[0].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
 
-                        int length = Children.Count;
+                        int length = children.Count;
                         for (int i = 1; i < length; i++)
                         {
-                            var right = Children[i].GetValue(variateGet);
+                            var right = children[i].GetValue(variateGet);
                             if (left == right)
                             {
                                 result = 1;
@@ -163,12 +172,12 @@ namespace FGUFW
                 case TextRulesHelper.Code_Out:
                     {
                         result = 1;
-                        var left = Children[0].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
 
-                        int length = Children.Count;
+                        int length = children.Count;
                         for (int i = 1; i < length; i++)
                         {
-                            var right = Children[i].GetValue(variateGet);
+                            var right = children[i].GetValue(variateGet);
                             if (left == right)
                             {
                                 result = 0;
@@ -180,79 +189,79 @@ namespace FGUFW
                 //----------------------------------------------------
                 case TextRulesHelper.Code_Equal:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left == right ? 1 : 0;
                     }
                     break;
                 case TextRulesHelper.Code_NotEqual:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left != right ? 1 : 0;
                     }
                     break;
                 case TextRulesHelper.Code_Greater:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left > right ? 1 : 0;
                     }
                     break;
                 case TextRulesHelper.Code_Less:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left < right ? 1 : 0;
                     }
                     break;
                 case TextRulesHelper.Code_GreaterAndEqual:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left >= right ? 1 : 0;
                     }
                     break;
                 case TextRulesHelper.Code_LessAndEqual:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left <= right ? 1 : 0;
                     }
                     break;
                 //----------------------------------------------------
                 case TextRulesHelper.Code_Add:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left + right;
                     }
                     break;
                 case TextRulesHelper.Code_Subtract:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left - right;
                     }
                     break;
                 case TextRulesHelper.Code_Multiply:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left * right;
                     }
                     break;
                 case TextRulesHelper.Code_Divide:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left / right;
                     }
                     break;
                 case TextRulesHelper.Code_Remainder:
                     {
-                        var left = Children[0].GetValue(variateGet);
-                        var right = Children[1].GetValue(variateGet);
+                        var left = children[0].GetValue(variateGet);
+                        var right = children[1].GetValue(variateGet);
                         result = left % right;
                     }
                     break;
@@ -261,6 +270,105 @@ namespace FGUFW
 
 
             return result;
+        }
+
+        /// <summary>
+        /// 获取所有叶子节点（即纯变量或纯常量），顺序严格对应文本从左到右
+        /// </summary>
+        public void GetAllLeafNodes(List<TextRules> resultList)
+        {
+            // 如果没有子节点，说明自己是底层数据（变量或常量）
+            if (children == null || children.Count == 0)
+            {
+                resultList.Add(this);
+                return;
+            }
+
+            // 如果有子节点，按顺序遍历（天然等同于从左到右）
+            foreach (var child in children)
+            {
+                child.GetAllLeafNodes(resultList);
+            }
+        }
+
+        /// <summary>
+        /// 自定义解析格式化文本 例:
+        /// </summary>
+        public string ToString( object variateGet,string format)
+        {
+            var cache = ListPool<TextRules>.Get();
+            GetAllLeafNodes(cache);
+
+            // 2. 
+            var result = new StringBuilder();
+
+            int len = format.Length;
+            int lastPos = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                // 3. 扫描左括号
+                if (format[i] == '{')
+                {
+                    // 把括号之前的纯文本追加进去
+                    if (i > lastPos)
+                    {
+                        result.Append(format, lastPos, i - lastPos);
+                    }
+
+                    // 4. 寻找右括号
+                    int endIdx = format.IndexOf('}', i);
+                    if (endIdx != -1)
+                    {
+                        // 5. 高效提取括号内的索引（不使用 int.Parse，避免产生垃圾）
+                        int index = 0;
+                        bool isValidNum = true;
+                        for (int j = i + 1; j < endIdx; j++)
+                        {
+                            char c = format[j];
+                            if (c >= '0' && c <= '9')
+                            {
+                                index = index * 10 + (c - '0');
+                            }
+                            else
+                            {
+                                isValidNum = false;
+                                break;
+                            }
+                        }
+
+                        // 6. 如果索引合法，直接把 float 追加进 StringBuilder
+                        if (isValidNum && index >= 0 && index < cache.Count)
+                        {
+                            float val = cache[index].GetValue(variateGet);
+                            
+                            // 关键点：StringBuilder 原生支持无装箱的 Append(float)
+                            // 注：如果想控制小数位数，可以写成 s_SB.Append(val.ToString("F1"))，但 ToString 也会有微量 GC。
+                            // 在现代 Unity 中，直接 Append(float) 是最经济的。
+                            result.Append(val); 
+                        }
+                        else
+                        {
+                            // 如果括号里不是数字（比如非主流文本），原样输出
+                            result.Append(format, i, endIdx - i + 1);
+                        }
+
+                        // 移动扫描指针
+                        i = endIdx;
+                        lastPos = i + 1;
+                    }
+                }
+            }
+
+            // 7. 追加尾部剩余的文本
+            if (lastPos < len)
+            {
+                result.Append(format, lastPos, len - lastPos);
+            }
+
+            ListPool<TextRules>.Release(cache);
+
+            return result.ToString();
         }
 
     }
