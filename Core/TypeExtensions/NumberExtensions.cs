2ba1d111de7c39734e595761a9707b6a9a69f691
飞鸽巴巴塔 <feigebabata@outlook.com>
2026-06-16 14:57:13 +0800

    Refactor NumberExtensions methods for simplicity

--- Core/TypeExtensions/NumberExtensions.cs
+++ Core/TypeExtensions/NumberExtensions.cs
@@ -4,53 +4,23 @@ namespace FGUFW
 {
     public static class NumberExtensions
     {
-        const string abbrCode = "KMBTqQsSOND"; //10^33
 
-        /// <summary>
-        /// 金融/统计类数值单位
-        /// </summary>
-        /// <param name="self"></param>
-        /// <returns>余数和后缀</returns>
-        public static (decimal,char) ToAbbr(this decimal self)
+        public static int Ads(this int self)
         {
-            decimal util = 1000;
-            char endCode = default;
-
-            for (int i = 0; i < abbrCode.Length; i++)
+            if(self<0)
             {
-                if(self<util)break;
-
-                self = self/util;
-                endCode = abbrCode[i];
+                return self * -1;
             }
-
-            return (self,endCode);
+            return self;
         }
 
-        public static (int,char) ToAbbr(this int self)
+        public static float Ads(this float self)
         {
-            decimal num = self;
-            var (n,c) = num.ToAbbr();
-            return ((int)n,c);
-        }
-
-        public static (double,char) ToAbbr(this double self)
-        {
-            decimal num = (decimal)self;
-            var (n,c) = num.ToAbbr();
-            return ((double)n,c);
-        }
-
-        public static (float,char) ToAbbr(this float self)
-        {
-            decimal num = (decimal)self;
-            var (n,c) = num.ToAbbr();
-            return ((float)n,c);
-        }
-
-        public static int ti(this float self)
-        {
-            return (int)self;
+            if(self<0)
+            {
+                return self * -1;
+            }
+            return self;
         }
 
     }
